9c815feb30ef4239bb2b640048f1e96c84cd5dc3
teredet <artem.vanzytler@gmail.com>
2021-05-19 10:40:24 +0300

    Add DetailView of task

--- base/views.py
+++ base/views.py
@@ -1,5 +1,16 @@
+from base.models import Task
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
-def TaskList(request):
-    return HttpResponse('To Do list')
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    template_name = 'base/task.html'
+    context_object_name = 'task'
